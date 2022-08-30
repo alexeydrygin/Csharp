64ddfd680f802abc09e8f1dbddb0ecb3d674a3f0
drygin_alexey <drygin.alex@mail.ru>
2022-08-30 21:13:05 +0700

    Написал программу приветствия пользователя и любимчика

---
+++ Example006_Condition_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower - позволяет все символы строки перевести в нижний регистр
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
