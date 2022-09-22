158dfd1cbc2fc03eb5c9d99aaa1a3d254a45087c
drygin_alexey <drygin.alex@mail.ru>
2022-09-22 17:28:25 +0700

    Семинар 6

---
+++ Seminares/Sem6Task39/Program.cs
@@ -0,0 +1,66 @@
+﻿//Задача №39
+// Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на
+// первом месте, а первый - на последнем и т.д.)
+// Например:
+// [1 2 3 4 5] -> [5 4 3 2 1] [6 7 3 6] -> [6 3 7 6]
+// Комментарий: эту задачу можно решить 2 способами: 1)
+// менять числа местами в исходном массиве; 2) создать
+// новый массив и в него записать перевёрнутый исходный
+// массив по элементам.
+
+//Метод заполнения массива
+int[] GenArray(int arrLength, int start, int stop)
+{
+    int[] array = new int[arrLength];
+    Random ren = new Random();
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        array[i] = ren.Next(start, stop + 1);
+    }
+    return array;
+}
+
+//Печатаем одномерный массив
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+
+//Метод создает новый массив и записывает в него перевернутый массив
+int[] SwapNewArray(int[] arr)
+{
+    int[] outArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        outArr[i] = arr[arr.Length - 1 - i];
+    }
+    return outArr;
+}
+
+//Метод разворота массива 
+int[] SwapArray(int[] arr)
+{
+    int bufElem = 0;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        bufElem = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = arr[i];
+        arr[i] = bufElem;
+    }
+    return arr;
+}
+
+int[] arr = GenArray(10, 1, 100);
+Console.WriteLine("Исходный массив");
+PrintArray(arr);
+int[] copyArray = SwapNewArray(arr);
+Console.WriteLine("Новый перевернутый массив");
+PrintArray(copyArray);
+
+Console.WriteLine("Исходный массив");
+PrintArray(arr);
+arr = SwapArray(arr);
+Console.WriteLine("Перевернутый исходный массив");
+PrintArray(arr);
---
+++ Seminares/Sem6Task40/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача №40
+// Напишите программу, которая принимает на вход
+// три числа и проверяет, может ли существовать
+// треугольник с сторонами такой длины.
+
+//Семинар с 0-19
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    //Выводим сообщение
+    Console.WriteLine(line);
+    //Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    //Возвращаем значение
+    return number;
+}
+
+//Метод вывода данных
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+//Вариант-1
+// bool TrglTest(int a, int b, int c)
+// {
+//     return (a + b > c) && (a + c > b) && (b + c > a);
+// }
+
+//Вариант-2
+bool TrglTest(int a, int b, int c)
+{
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+int a = ReadData("Введите длину стороны a: ");
+int b = ReadData("Введите длину стороны b: ");
+int c = ReadData("Введите длину стороны c: ");
+
+PrintResult(TrglTest(a, b, c) ? "Такой треугольник существует" : "Такой треугольник не существует");
