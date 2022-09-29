95d876e09577386de9724d8444123716a29eeff0
drygin_alexey <drygin.alex@mail.ru>
2022-09-29 11:41:54 +0700

    Семинар 9 задача 67

---
+++ Seminares/Sem9Task67/Program.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 67
+// примает на вход число и возвращает сумму числа
+// ---------------------------------------------------------------------
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(string prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int RecSumDig(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + RecSumDig(num / 10);
+}
+
+int numN = ReadData("Введите число: ");
+
+PrintResult(RecSumDig(numN).ToString());
