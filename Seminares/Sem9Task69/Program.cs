4a22c4232235947e864d50cbad12ecf64dc0201d
drygin_alexey <drygin.alex@mail.ru>
2022-09-29 11:45:25 +0700

    Семинар 9 задача 69

---
+++ Seminares/Sem9Task69/Program.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 69
+// принимает на вход числа и возвращает степень числа
+// ---------------------------------------------------------------------
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int RecPow(int A, int B)
+{
+    if (B == 1) return A;
+    return A * RecPow(A, B - 1);
+}
+
+int NoRecPow(int num, int pow)
+{
+    int result = 1;
+    for (int i = 0; i < pow; i++)
+    {
+        result = result * num;
+    }
+    return result;
+}
+
+int A = ReadData("Введите число: ");
+int B = ReadData("Введите степень: ");
+
+PrintResult(RecPow(A, B));
