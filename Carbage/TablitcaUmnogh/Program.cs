cb37ffca0e22761ba59fcb897a6cb38f68637bbd
drygin_alexey <drygin.alex@mail.ru>
2022-10-24 13:23:12 +0700

    Программка для создания таблицы умножения

---
+++ Carbage/TablitcaUmnogh/Program.cs
@@ -0,0 +1,15 @@
+﻿//Таблица умножения
+
+Console.WriteLine("Введите количество цифр для перемножения");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        Console.Write(i * j);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
